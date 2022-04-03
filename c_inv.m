b5989c469334817445913dfd74ae5e085de16afc
Pedro Martinez <pedroomtz17@gmail.com>
2022-04-03 17:52:10 -0500

    Agregué comentarios sobre #ecuaciones

--- c_inv.m
+++ c_inv.m
@@ -3,6 +3,8 @@ q1=q(1); q2=q(2); q3=q(3); q4=q(4); q5=q(5); q6=q(6);
 global xe ye ze alfa betha gamma
 global l1 l2 l3 l4
 
+%Hay 6 ecuaciones debido a que hay 
+%6 variables articulares (qn)
 F=[
 %ecu1 =
 cos(gamma)*sin(alfa)*sin(betha) - cos(q6)*(cos(q5)*(sin(q1)*sin(q4) + cos(q4)*(cos(q1)*sin(q3)*sin(q2 + pi/2) - cos(q1)*cos(q3)*cos(q2 + pi/2))) + sin(q5)*(cos(q1)*cos(q3)*sin(q2 + pi/2) + cos(q1)*cos(q2 + pi/2)*sin(q3))) - cos(alfa)*cos(betha)*cos(gamma) - sin(q6)*(cos(q4)*sin(q1) - sin(q4)*(cos(q1)*sin(q3)*sin(q2 + pi/2) - cos(q1)*cos(q3)*cos(q2 + pi/2)));
