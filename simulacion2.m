7af9d966a24acddf4ee8847109152e8d92cb531c
Pedro Martinez <pedroomtz17@gmail.com>
2022-04-03 19:17:41 -0500

    Agregué comentarios

--- simulacion2.m
+++ simulacion2.m
@@ -3,9 +3,10 @@ parametros
 
 global xe ye ze alfa betha gamma
 
-xe=0.45;ye=0.45;ze=0.45;
+%Posición y orientación de efector
+xe=0.45;ye=0.45;ze=0.45; 
 alfa=0; betha=0;gamma=0;
 
-q0=[0,0,1,0,0,1];
-q=fsolve(@c_inv,q0)
+q0=[0,0,1,0,0,1]; %Condición inicial
+q=fsolve(@c_inv,q0)%Función para el método numérico
 robot(q(1),q(2),q(3),q(4),q(5),q(6))
